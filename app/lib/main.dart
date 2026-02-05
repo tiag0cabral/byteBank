a248c3739d131d7f12e87eceb346d64b2d7553f8
tiag0cabral <tiago.cabral@eb.mil.br>
2026-02-05 11:03:18 -0300

    Adicionando a lista de transferências

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,30 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: const Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100,00'),
-                subtitle: Text('4346-X'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200,00'),
-                subtitle: Text('4346-X'),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencia(),
         appBar: AppBar(
           title: const Text('Transferências'),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          child: Icon(Icons.add),
+          child: const Icon(Icons.add),
         ),
       ),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 4346)),
+        ItemTransferencia(Transferencia(200.0, 4347)),
+        ItemTransferencia(Transferencia(300.0, 4348)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.idTransacao.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final double idTransacao;
+
+  const Transferencia(this.valor, this.idTransacao);
+}
