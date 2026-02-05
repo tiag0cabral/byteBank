69352599d3c99b5fe41c85723f51a8e501d2a26f
tiag0cabral <tiago.cabral@eb.mil.br>
2026-02-05 08:53:28 -0300

    Implementando o design do item de transferência

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: const Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100,00'),
+                subtitle: Text('4346-X'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('200,00'),
+                subtitle: Text('4346-X'),
+              ),
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: const Text('Transferências'),
         ),
